c31d5d22708bef9f0d03a80e052c7b9511c6d029
Héctor <hectorcrespuig@gmail.com>
2020-11-25 20:37:19 +0100

    Inicio fase2 proyecto

---
+++ src/main/java/DAO/BasicDataDAO.java
@@ -0,0 +1,125 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import model.BasicData;
+
+/**
+ *
+ * @author HECTOR
+ */
+public class BasicDataDAO {
+    private Connection con;
+    private BasicData basicData;
+
+    public BasicDataDAO(Connection con, BasicData basicData) {
+        this.con = con;
+        this.basicData = basicData;
+    }
+
+    public BasicDataDAO(Connection con) {
+        this.con = con;
+    }
+
+    public int insertaBasicData() throws Exception {
+        PreparedStatement stmt = null;
+
+        try {
+
+            stmt = con.prepareStatement("INSERT INTO basicdata (language, phone, web)"
+                    + "VALUES (?,?,?,?,?,?,?,?);");
+            stmt.setString(1, basicData.getLanguage());
+            stmt.setString(2, basicData.getPhone());
+            stmt.setString(3, basicData.getWeb());
+            
+
+            return stmt.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            throw new Exception("Problema al insertar basicData.");
+        } finally {
+            if (stmt != null) {
+                stmt.close();
+            }
+        }
+    }
+
+    public int actualiza() throws SQLException {
+        PreparedStatement stmt = null;
+
+        stmt = con.prepareStatement("UPDATE basicdata SET language=?, phone=? "
+                + "web=?");
+        stmt.setString(1, basicData.getLanguage());
+        stmt.setString(2, basicData.getPhone());
+        stmt.setString(3, basicData.getTitle());
+       
+        return stmt.executeUpdate();
+
+    }
+
+    public int elimina() throws SQLException {
+        PreparedStatement stmt = null;
+        stmt = con.prepareStatement("DELETE FROM basicdata WHERE idBasicData=?");
+        stmt.setInt(1, basicData.getIdBasicData());
+        return stmt.executeUpdate();
+    }
+
+    public BasicData buscaBasicDataById(int id) throws SQLException, Exception {
+        BasicData basicdata = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        try {
+            stmt = con.prepareStatement("SELECT * FROM basicdata WHERE idBasicData=?");       
+            stmt.setInt(1, id);
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                basicdata = new BasicData();
+                basicdata.setIdBasicData(rs.getInt("idBasicData"));
+                basicdata.setLanguage(rs.getString("language"));
+                basicdata.setPhone(rs.getString("phone"));
+                basicdata.setWeb(rs.getString("web"));
+            }
+            return basicdata;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            throw new Exception("Ha habido un problema al buscar el BasicData");
+        }finally{
+            if (stmt != null) stmt.close();
+        }
+        
+    
+
+    }
+
+    public String recuperaClientes() throws SQLException, Exception {
+        String lista = "Lista de BasicData: \n-----------------\n";
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        try {
+            stmt = con.prepareStatement("SELECT * FROM basicdata");
+
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                lista += rs.getInt("idBasicData") + " -> " + rs.getString("language") + " " + rs.getString("phone") + " " + rs.getString("web") + "\n";
+
+            }
+            return lista;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            throw new Exception("Ha habido un problema al recuperar los BasicData");
+        } finally {
+            if (stmt != null) {
+                stmt.close();
+            }
+        }
+
+    }
+}
+
--- src/main/java/model/BasicData.java
+++ src/main/java/model/BasicData.java
@@ -5,16 +5,42 @@
  */
 package model;
 
+import DAO.BasicDataDAO;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 /**
  *
  * @author HECTOR
  */
 public class BasicData {
+
     private String language;
     private String phone;
     private String web;
+    private int idBasicData;
+
+    public String getWeb() {
+        return web;
+    }
+
+    public void setWeb(String web) {
+        this.web = web;
+    }
+
+    public int getIdBasicData() {
+        return idBasicData;
+    }
+
+    public void setIdBasicData(int idBasicData) {
+        this.idBasicData = idBasicData;
+    }
 
     public BasicData() {
+    }
+
+    public BasicData(int idBasicData) {
+        this.idBasicData = idBasicData;
     }
 
     public BasicData(String language, String phone, String web) {
@@ -23,11 +49,37 @@ public class BasicData {
         this.web = web;
     }
 
-    public String getName() {
+    public int insertaClienteBBDD(Connection con) throws SQLException, Exception {
+        BasicDataDAO cliDao = new BasicDataDAO(con, this);
+        return cliDao.insertaBasicData();
+    }
+
+    public int eliminarClienteBBDD(Connection con) throws SQLException, Exception {
+        BasicDataDAO cliDAO = new BasicDataDAO(con, this);
+        return cliDAO.elimina();
+    }
+
+    public int actualizarClienteBBDD(Connection con) throws SQLException, Exception {
+        BasicDataDAO cliDAO = new BasicDataDAO(con, this);
+        return cliDAO.actualiza();
+    }
+
+    /*public Cliente buscaClienteByDni(Connection con) throws SQLException{
+        ClienteDAO cliDao = new ClienteDAO(con, this);
+        Cliente cli;
+        cli = cliDao.buscaClienteByDni(this.getDni());
+        return cli;
+    }*/
+    public void modificaCliente(Connection con) throws SQLException {
+        BasicDataDAO cliDao = new BasicDataDAO(con, this);
+        cliDao.actualiza();
+    }
+
+    public String getLanguage() {
         return language;
     }
 
-    public void setName(String name) {
+    public void setLanguage(String name) {
         this.language = name;
     }
 
@@ -51,6 +103,5 @@ public class BasicData {
     public String toString() {
         return "\n\t\tLanguage: " + language + "\n\t\tPhone: " + phone + "\n\t\tWeb: " + web;
     }
-    
-    
+
 }
